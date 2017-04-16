d1913e0737514e7e06af84166e5850439289a54c
Daman Bir Singh <damanBirSingh97@gmail.com>
2017-04-16 20:02:42 +0530

    handles space in file name

--- Apk_installer/install_apk.sh
+++ Apk_installer/install_apk.sh
@@ -1,7 +1,11 @@
 for file in *.apk ;do
-	for f in *\ *; do 
-		mv "$f" "${f// /_}"; 
-	done
+	echo "Installing $file"
+	if [[ $file != "${file/ /}" ]]
+		then
+		for file in *\ *; do 
+			mv "$file" "${file/ /}"; 
+		done
+	fi
 	adb install $file
 done
-echo $n apps installed
+echo "Script complete"
