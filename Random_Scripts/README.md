29fbd684160b390100dbf0b160804ea77f296858
Daman Bir Singh <damanBirSingh97@gmail.com>
2021-01-03 09:23:48 +0530

    added a install script

--- Random_Scripts/README.md
+++ Random_Scripts/README.md
@@ -1,1 +1,4 @@
 ## Some small and simple scripts I use on a day to basis.
+
+
+* Running the install.sh will add sysbolic links to all the scipts to your /usr/local/bin dir. Comment out the ones you don't want before executing it.
---
+++ Random_Scripts/install.sh
@@ -0,0 +1,7 @@
+# Place these scripts as symbolic links to your /usr/local/bin so that these can be executed from anywhere.
+# Make sure your $PATH variable has /usr/local/bin. (which is generally has)
+# Comment out the ones you don't want to add.
+
+echo 'sudo ln -s ~/System_Initial_Setup/Random_Scripts/Virtual_cam_dev.sh /usr/local/bin/virtual_cam_new' | /usr/bin/bash
+echo 'sudo ln -s ~/System_Initial_Setup/Random_Scripts/sleep_pc.sh /usr/local/bin/sleep_pc' | /usr/bin/bash
+echo 'sudo ln -s ~/System_Initial_Setup/Random_Scripts/unlockPhoneadb.sh /usr/local/bin/adb_phone_unlock' | /usr/bin/bash
