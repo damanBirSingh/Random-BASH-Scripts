f236e58163d55d44ddf376fce7b6173f299bf904
Daman Bir Singh <damanBirSingh97@gmail.com>
2021-02-23 22:07:27 +0530

    push before system change

--- Random_Scripts/install.sh
+++ Random_Scripts/install.sh
@@ -1,7 +1,7 @@
-# Place these scripts as symbolic links to your /usr/local/bin so that these can be executed from anywhere.
-# Make sure your $PATH variable has /usr/local/bin. (which is generally has)
+# Places these scripts as symbolic links to your /usr/local/bin so that these can be executed from anywhere.
+# Make sure your $PATH variable has /usr/local/bin. (which it generally has)
 # Comment out the ones you don't want to add.
 
-echo 'sudo ln -s ~/System_Initial_Setup/Random_Scripts/virtual_cam_new.sh /usr/local/bin/virtual_cam_new' | /usr/bin/bash
-echo 'sudo ln -s ~/System_Initial_Setup/Random_Scripts/sleep_pc.sh /usr/local/bin/sleep_pc' | /usr/bin/bash
-echo 'sudo ln -s ~/System_Initial_Setup/Random_Scripts/adb_phone_unlock.sh /usr/local/bin/adb_phone_unlock' | /usr/bin/bash
+echo 'sudo ln -sfn $(readlink -f virtual_cam_new.sh) /usr/local/bin/virtual_cam_new' | /usr/bin/bash
+echo 'sudo ln -sfn $(readlink -f sleep_pc.sh) /usr/local/bin/sleep_pc' | /usr/bin/bash
+echo 'sudo ln -sfn $(readlink -f adb_phone_unlock.sh) /usr/local/bin/adb_phone_unlock' | /usr/bin/bash
